a610a306d3aa7cbd97a006b9d1476feed3eeb4b8
Winnie Zhao <Winnie.Zhao@MacGregorPartners.com>
2016-09-20 00:44:26 -0500

    20160920

--- LeetCodePractice/src/LeetCode/Medium/MaximumProductOfWordLengths.java
+++ LeetCodePractice/src/LeetCode/Medium/MaximumProductOfWordLengths.java
@@ -30,6 +30,44 @@ package LeetCode.Medium;
 public class MaximumProductOfWordLengths {
 
     public int maxProduct(String[] words) {
+        if (words == null || words.length == 0) {
+            return 0;
+        }
         
+        int length = words.length;
+        int[] mark = new int[length];
+        for (int i=0; i<length; i++) {
+            String word = words[i];
+            
+            /**
+             * Using Bit manipulation:
+             * 1 << 0 (when char='a'): 1 move left 0 position = 1
+             * 1 << 1 (when char='b'): 1 move left 1 position = 10 = 2
+             */
+            for (int j=0; j<word.length(); j++) {
+                char w = word.charAt(j);
+                
+                int digit = w - 'a';
+                digit = 1 << digit;
+                mark[i] = mark[i] | digit;
+            }
+        }
+        
+        int maxProduct = 0;
+        for (int i=0; i<length; i++) {
+            for (int j=i+1; j<length; j++) {
+                if ((mark[i] & mark[j]) == 0 && (words[i].length() * words[j].length() > maxProduct)) {
+                    maxProduct = words[i].length() * words[j].length();
+                }
+            }
+        }
+        
+        return maxProduct;
+    }
+    
+    public static void main(String[] args) {
+        MaximumProductOfWordLengths solution = new MaximumProductOfWordLengths();
+        String[] words = {"ab", "acd"};
+        System.out.println(solution.maxProduct(words));
     }
 }
