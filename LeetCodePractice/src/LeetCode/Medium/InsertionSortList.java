6e4f72e749aeb82d495b63189f74e393c188927e
Winnie Zhao <Winnie.Zhao@MacGregorPartners.com>
2016-10-26 02:30:56 -0500

    20161025

---
+++ LeetCodePractice/src/LeetCode/Medium/InsertionSortList.java
@@ -0,0 +1,59 @@
+package LeetCode.Medium;
+
+import LeetCode.Helper.ListNode;
+
+/**
+ * Sort a linked list using insertion sort.
+ * 
+ * @author WinnieZhao
+ *
+ */
+public class InsertionSortList {
+
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode dummy = new ListNode(0);
+
+        ListNode pre = dummy;
+        ListNode cur = head;
+        
+        while (cur != null) {
+            pre = dummy;
+            
+            while(pre.next != null && pre.next.val < cur.val) {
+                pre = pre.next;
+            }
+            
+            ListNode next = cur.next;
+            cur.next = pre.next;
+            pre.next = cur;
+            cur = next;
+        }
+        
+        return dummy.next;
+    }
+    
+    public static void main(String[] args) {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(4);
+        ListNode third= new ListNode(3);
+        ListNode fourth = new ListNode(2);
+        ListNode fifth = new ListNode(5);
+        ListNode sixth = new ListNode(2);
+        
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        
+        InsertionSortList solution = new InsertionSortList();
+        ListNode head = solution.insertionSortList(first);
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+    
+}
