4baa8b6aa4bb0b0eac80fa3d63a35532938f2ff4
winniezhao <winnie.zhao@macgregorpartners.com>
2017-03-22 16:32:43 -0500

    20170322

--- LeetCodePractice/src/LeetCode/Medium/Minesweeper.java
+++ LeetCodePractice/src/LeetCode/Medium/Minesweeper.java
@@ -1,16 +1,35 @@
 package LeetCode.Medium;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
+ * 1. If click on a mine ('M'), mark it as 'X', stop further search.
+ * 2. If click on an empty cell ('E'), depends on how many surrounding mine:
+ * 2.1: Has surrounding mine(s), mark it with number of surrounding mine(s), stop further search.
+ * 2.2: No surrounding mine, mark it as 'B', continue search its 8 neighbors.
+
  * Created by WinnieZhao on 2017/3/22.
  */
 public class Minesweeper {
-
     int row;
     int column;
+    int[][]direction = {{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,1},{-1, 1},{1,-1}};
+    char[][] board;
+    class Board {
+        int x;
+        int y;
+        Board(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     public char[][] updateBoard(char[][] board, int[] click) {
         int x = click[0];
         int y = click[1];
 
+        this.board = board;
         row = board.length;
         column = board[0].length;
 
@@ -18,41 +37,75 @@ public class Minesweeper {
             board[x][y] = 'X';
             return board;
         }
+        
+        Queue<Board> queue = new LinkedList<>();
+        queue.add(new Board(x, y));
+        
+        while (!queue.isEmpty()) {
+            Board bd = queue.poll();
+            int m = bd.x;
+            int n = bd.y;
+            
+            if (board[m][n] == 'M') {
+                board[m][n] = 'X';
+            }
 
-        this.updateBoard(board, x, y);
+            if (board[m][n] == 'E') {
+                this.markMine(queue, m, n);
+            }
+        }
 
         return board;
     }
-
-    private void updateBoard(char[][]board, int i, int j) {
-        if (i<0 || i>=row || j<0 || j>=column) {
-            return;
+    
+    private void markMine(Queue<Board> queue, int i, int j) {
+        int mineCount = 0;
+        for (int[] dir : direction) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            
+            if (x<0 || y <0 || x>=row || y>=column) {
+                continue;
+            }
+            
+            if (board[x][y] == 'M') {
+                mineCount++;
+            }
         }
-
-        if (board[i][j] == 'E') {
+        if (mineCount > 0) {
+            board[i][j] = (char) (mineCount + '0');
+        }
+        else {
             board[i][j] = 'B';
-
-            updateBoard(board, i-1, j);
-            updateBoard(board, i+1, j);
-            updateBoard(board, i, j-1);
-            updateBoard(board, i, j+1);
-
-            updateBoard(board, i-1, j-1);
-            updateBoard(board, i+1, j+1);
-            updateBoard(board, i-1, j+1);
-            updateBoard(board, i+1, j-1);
+            this.addAdjChars(queue, i, j);
         }
-        else if (board[i][j] == 'M') {
-            board[i-1][j] = '1';
-            board[i+1][j] = '1';
-            board[i][j-1] = '1';
-            board[i][j+1] = '1';
-
-            board[i-1][j-1] = '1';
-            board[i+1][j+1] = '1';
-            board[i-1][j+1] = '1';
-            board[i+1][j-1] = '1';
-            return;
+    }
+    
+    private void addAdjChars(Queue<Board> queue, int i, int j) {
+        for (int[] dir : direction) {
+            
+            int x = i + dir[0];
+            int y = j + dir[1];
+            
+            if (x<0 || y <0 || x>=row || y>=column) {
+                continue;
+            }
+            queue.add(new Board(x, y));
+        }
+        return;
+    }
+    
+    public static void main(String[] args) {
+        Minesweeper solution = new Minesweeper();
+        char[][] board = {{'E', 'E', 'E', 'E', 'E'},
+                          {'E', 'E', 'M', 'E', 'E'},
+                          {'E', 'E', 'E', 'E', 'E'},
+                          {'E', 'E', 'E', 'E', 'E'}};
+        int[] click = {3,0};
+        
+        char[][] result = solution.updateBoard(board, click);
+        for (char[] row : result) {
+            System.out.println(Arrays.toString(row));
         }
     }
 }
