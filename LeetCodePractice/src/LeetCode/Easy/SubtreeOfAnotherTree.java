d176eb4ac32620e7b417b1731661cbac4e62ca4a
Winnie Zhao <Winnie.Zhao@MacGregorPartners.com>
2017-05-10 01:28:10 -0500

    20170510

---
+++ LeetCodePractice/src/LeetCode/Easy/SubtreeOfAnotherTree.java
@@ -0,0 +1,75 @@
+package LeetCode.Easy;
+
+import LeetCode.Helper.TreeNode;
+
+/**
+ * Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s.
+ * A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+ *
+ * Example 1:
+ * Given tree s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ *
+ *  Given tree t:
+ *    4
+ *   / \
+ *  1   2
+ *
+ *  Return true, because t has the same structure and node values with a subtree of s
+ *
+ *  Example 2:
+ *  Given tree s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ *     /
+ *    0
+ *
+ * Given tree t:
+ *    4
+ *   / \
+ *  1   2
+ *
+ *  Return false.
+
+ * Created by WinnieZhao on 2017/5/8.
+ */
+public class SubtreeOfAnotherTree {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        String s1 = this.inorder(s);
+        String s2 = this.inorder(t);
+
+        return s1.contains(s2);
+    }
+
+    private String inorder(TreeNode root) {
+        if (root == null) {
+            return ",#";
+        }
+        String s = ","+root.val;
+        s += this.inorder(root.left);
+        s += this.inorder(root.right);
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        TreeNode s = new TreeNode(12);
+        TreeNode t = new TreeNode(2);
+
+        SubtreeOfAnotherTree solution = new SubtreeOfAnotherTree();
+
+        System.out.println(solution.inorder(s));
+        System.out.println(solution.inorder(t));
+        System.out.println(solution.isSubtree(s, t));
+    }
+}
---
+++ LeetCodePractice/src/LeetCode/Hard/InterleavingString.java
@@ -0,0 +1,44 @@
+package LeetCode.Hard;
+
+/**
+ * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+ *
+ * For example,
+ * Given:
+ * s1 = "aabcc",
+ * s2 = "dbbca",
+ *
+ * When s3 = "aadbbcbcac", return true.
+ * When s3 = "aadbbbaccc", return false.
+
+ * Created by WinnieZhao on 2017/5/9.
+ */
+public class InterleavingString {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s3.length() != s1.length() + s2.length()) {
+            return false;
+        }
+
+        boolean dp[][] = new boolean[s1.length()+1][s2.length()+1];
+
+        for (int i=0; i<=s1.length(); i++) {
+            for (int j=0; j<=s2.length(); j++) {
+                if (i==0 && j==0) {
+                    dp[i][j] = true;
+                }
+                else if (i==0) {
+                    dp[i][j] = dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1);
+                }
+                else if (j==0) {
+                    dp[i][j] = dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1);
+                }
+                else {
+                    dp[i][j] = (dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1)) || (dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1));
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
+}
