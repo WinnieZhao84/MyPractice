83e759c8a0dd98b656abe891b8eff001836509d7
winniezhao <winnie.zhao@macgregorpartners.com>
2017-05-23 16:09:35 -0500

    20170523

--- LeetCodePractice/src/LeetCode/Hard/DesignInMemoryFileSystem.java
+++ LeetCodePractice/src/LeetCode/Hard/DesignInMemoryFileSystem.java
@@ -1,5 +1,7 @@
 package LeetCode.Hard;
 
+import java.util.*;
+
 /**
  * Design an in-memory file system to simulate the following functions:
  *
@@ -26,10 +28,11 @@ package LeetCode.Hard;
  * Explanation:
  *            Operation                      Output        Explanation
  *  FileSystem fs = new FileSystem()          null         The constructor returns nothing
- *  fs.ls()                                   []          Initially, directory "/" has nothing, so return empty list
+ *  fs.ls(/)                                   []          Initially, directory "/" has nothing, so return empty list
  *  fs.mkdir("/a/b/c")                        null         Create directory a in directory /. Then create dir b in dir a. Finally
  *                                                         create dir c in dir b.
- *  fs.addContentToFile("/a/b/c/d", "hello")  null         Create a file named d with conrent "hello" in directory /a/b/c
+ *  fs.addContentToFile("/a/b/c/d", "hello")  null         Create a file named d with content "hello" in directory /a/b/c
+ *  fs.ls(/)                                   "a"         Only directory "a" in directory "/".
  *  fs.readContentFromFile("/a/b/c/d")       "hello"       Output the file content
  *
  * Note:
@@ -40,5 +43,78 @@ package LeetCode.Hard;
  * Created by WinnieZhao on 2017/5/23.
  */
 public class DesignInMemoryFileSystem {
+    /**
+     * Your FileSystem object will be instantiated and called as such:
+     * FileSystem obj = new FileSystem();
+     * List<String> param_1 = obj.ls(path);
+     * obj.mkdir(path);
+     * obj.addContentToFile(filePath,content);
+     * String param_4 = obj.readContentFromFile(filePath);
+     */
 
+     class Node {
+        String name;
+        boolean isFile;
+        StringBuilder content;
+        Map<String, Node> children;
+
+        Node(String name) {
+            this.content = new StringBuilder();
+            this.name = name;
+            this.children = new TreeMap<>();
+        }
+    }
+
+    Node root;
+    public DesignInMemoryFileSystem() {
+        root = new Node("/");
+    }
+
+    private Node traverse(String filePath) {
+        String[] arr = filePath.split("/");
+        Node cur = root;
+
+        // start from index 1 as 0th element is ""
+        for(int i = 1; i < arr.length; i++) {
+            if(!cur.children.containsKey(arr[i])) {
+                Node nd = new Node(arr[i]);
+                cur.children.put(arr[i], nd);
+            }
+
+            cur = cur.children.get(arr[i]);
+        }
+
+        return cur;
+    }
+
+    public List<String> ls(String path) {
+        Node node = this.traverse(path);
+
+        List<String> result = new ArrayList<>();
+        if (node.isFile) {
+            result.add(node.name);
+        }
+        else {
+            for (String name : node.children.keySet()) {
+                result.add(name);
+            }
+        }
+        return result;
+
+    }
+
+    public void mkdir(String path) {
+        this.traverse(path);
+    }
+
+    public void addContentToFile(String filePath, String content) {
+        Node node = this.traverse(filePath);
+        node.isFile = true;
+        node.content.append(content);
+    }
+
+    public String readContentFromFile(String filePath) {
+        Node node = this.traverse(filePath);
+        return node.content.toString();
+    }
 }
