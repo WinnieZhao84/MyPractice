1800581b237ec92d26bf42470ba1ec894477ba77
Winnie Zhao <Winnie.Zhao@MacGregorPartners.com>
2017-05-25 01:09:27 -0500

    20130524

---
+++ LeetCodePractice/src/LeetCode/Hard/PalindromePairs.java
@@ -0,0 +1,70 @@
+package LeetCode.Hard;
+
+import java.util.*;
+
+/**
+ * Given a list of unique words, find all pairs of distinct indices (i, j) in the given list,
+ * so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
+ *
+ * Example 1: Given words = ["bat", "tab", "cat"]
+ * Return [[0, 1], [1, 0]]
+ * The palindromes are ["battab", "tabbat"]
+ *
+ * Example 2: Given words = ["abcd", "dcba", "lls", "s", "sssll"]
+ * Return [[0, 1], [1, 0], [3, 2], [2, 4]]
+ * The palindromes are ["dcbaabcd", "abcddcba", "slls", "llssssll"]
+
+ * Created by WinnieZhao on 2017/5/25.
+ */
+public class PalindromePairs {
+
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (words == null || words.length < 2) return result;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i=0; i<words.length; i++) {
+            map.put(words[i], i);
+        }
+
+        for (int i=0; i<words.length; i++) {
+            for (int j=0; j<=words[i].length(); j++) { // notice it should be "j <= words[i].length()"
+                String left = words[i].substring(0, j);
+                String right = words[i].substring(j);
+
+                if (isPalindrome(left)) {
+                    String rightReverse = new StringBuilder(right).reverse().toString();
+
+                    if (map.getOrDefault(rightReverse, i) != i) {
+                        result.add(Arrays.asList(map.get(rightReverse), i));
+                    }
+                }
+                if (isPalindrome(right) && right.length() != 0) {
+                    String leftReverse = new StringBuilder(left).reverse().toString();
+                    // check "str.length() != 0" to avoid duplicates
+
+                    if (map.getOrDefault(leftReverse, i) != i) {
+                        result.add(Arrays.asList(i, map.get(leftReverse)));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean isPalindrome(String s) {
+        int start=0;
+        int end=s.length()-1;
+
+        while (start<end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
--- LeetCodePractice/src/LeetCode/Hard/PalindromePartitioningII.java
+++ LeetCodePractice/src/LeetCode/Hard/PalindromePartitioningII.java
@@ -1,5 +1,7 @@
 package LeetCode.Hard;
 
+import java.util.Arrays;
+
 /**
  * Given a string s, partition s such that every substring of the partition is a palindrome.
  *
@@ -11,24 +13,55 @@ package LeetCode.Hard;
  */
 public class PalindromePartitioningII {
 
+    /**
+     * Calculate and maintain 2 DP states:
+     *
+     * pal[i][j] , which is whether s[i..j] forms a pal
+     * d[i], which is the minCut for s[i..n-1]
+     *
+     * Once we comes to a pal[i][j]==true:
+     * if j==n-1, the string s[i..n-1] is a Pal, minCut is 0, d[i]=0;
+     * else: the current cut num (first cut s[i..j] and then cut the rest s[j+1...n-1]) is 1+d[j+1], compare it to the existing minCut num
+     *       d[i], replace if smaller. d[0] is the answer.
+
+     * @param s
+     * @return
+     */
     public int minCut(String s) {
         if (s == null || s.isEmpty()) return 0;
 
+        int length = s.length();
+        boolean[][] palindrome= new boolean[length][length];
 
+        for (int i=0;i<length;i++) {
+            Arrays.fill(palindrome[i], false);
+        }
 
-    }
+        int[] results = new int[length];
+        for (int start=length-1; start>=0; start--) {
+            results[start] = length - start - 1;
 
-    private boolean isPalindrome(String s, int left, int right) {
+            for (int end=start; end<length; end++) {
 
-        while (s.charAt(left) == s.charAt(right)) {
-            left++;
-            right--;
+                if (s.charAt(start) == s.charAt(end)) {
+                    if (end-start < 2) //Refer to these two cases: xx OR xyx
+                        palindrome[start][end]=true;
+                    else //Depends on the inner substring,if the inner substring is palindrome,the outer is
+                        palindrome[start][end]=palindrome[start+1][end-1];
+                }
 
-            if (left >= right) {
-                return true;
+                if (palindrome[start][end]) {
+                    if (end==length-1) {
+                        results[start] = 0;
+                    }
+                    else {
+                        results[start] = Math.min(results[start], results[end + 1] + 1);
+                    }
+                }
+
             }
         }
 
-        return false;
+        return results[0];
     }
 }
