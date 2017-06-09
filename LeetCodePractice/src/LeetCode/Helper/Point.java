acdd246882a33f241f59c9e1cf1e442bb72b426c
winniezhao <winnie.zhao@macgregorpartners.com>
2017-06-09 16:46:36 -0500

    20170609

---
+++ LeetCodePractice/src/LeetCode/Helper/Point.java
@@ -0,0 +1,11 @@
+package LeetCode.Helper;
+
+/**
+ * Created by WinnieZhao on 6/9/2017.
+ */
+public class Point {
+    int x;
+    int y;
+    Point() { x = 0; y = 0; }
+    Point(int a, int b) { x = a; y = b; }
+}
