fcfb3d5960d9fad5b831d1df0858c8502fa672c5
Winnie Zhao <Winnie.Zhao@MacGregorPartners.com>
2017-07-06 01:28:31 -0500

    20170705

--- LeetCodePractice/src/LeetCode/Hard/RussianDollEnvelopes.java
+++ LeetCodePractice/src/LeetCode/Hard/RussianDollEnvelopes.java
@@ -1,5 +1,7 @@
 package LeetCode.Hard;
 
+import java.util.Arrays;
+
 /**
  * You have a number of envelopes with widths and heights given as a pair of integers (w, h).
  * One envelope can fit into another if and only if both the width and height of one envelope
@@ -26,5 +28,33 @@ public class RussianDollEnvelopes {
      */
     public int maxEnvelopes(int[][] envelopes) {
 
+        if (envelopes==null || envelopes.length==0) {
+            return 0;
+        }
+
+        Arrays.sort(envelopes, (int[] ev1, int[] ev2) -> {
+            if (ev1[0] != ev2[0]){
+                return ev1[0] - ev2[0];
+            }
+            else{
+                return ev1[1] - ev2[1];
+            }
+        });
+
+        int max = 1;
+        int[] dp = new int[envelopes.length];
+
+        for (int i=0; i<envelopes.length; i++) {
+            dp[i] = 1;
+
+            for (int j=i-1; j>=0; j--) {
+                if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
     }
 }
