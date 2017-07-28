d2923644fff1cfb236a1621df33bc51e237ac6bc
winniezhao <winnie.zhao@macgregorpartners.com>
2017-07-28 10:02:15 -0500

    Testing

--- LeetCodePractice/src/LeetCode/Easy/BinaryWatch.java
+++ LeetCodePractice/src/LeetCode/Easy/BinaryWatch.java
@@ -8,7 +8,9 @@ import java.util.Set;
 /**
  * A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59).
  * Each LED represents a zero or one, with the least significant bit on the right.
- * 
+ *
+ *    8   4  (2)   (1)
+ * 32 (16) (8) 4  2  (1)
  * For example, the above binary watch reads "3:25".
  * Given a non-negative integer n which represents the number of LEDs that are currently on, return all possible times the watch could represent.
  * 
