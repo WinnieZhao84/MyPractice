86b036b414bc81938621fa93077767b1230e7f9e
Winnie Zhao <Winnie.Zhao@MacGregorPartners.com>
2017-09-27 00:40:12 -0500

    20170926

---
+++ LeetCodePractice/src/LeetCode/Interview/Amazon/BaseballGame.java
@@ -0,0 +1,91 @@
+package LeetCode.Interview.Amazon;
+
+import java.util.Stack;
+
+/**
+ * You're now a baseball game point recorder.
+
+ Given a list of strings, each string can be one of the 4 following types:
+
+ Integer (one round's score): Directly represents the number of points you get in this round.
+ "+" (one round's score): Represents that the points you get in this round are the sum of the last two valid round's points.
+ "D" (one round's score): Represents that the points you get in this round are the doubled data of the last valid round's points.
+ "C" (an operation, which isn't a round's score): Represents the last valid round's points you get were invalid and should be removed.
+ Each round's operation is permanent and could have an impact on the round before and the round after.
+
+ You need to return the sum of the points you could get in all the rounds.
+
+ Example 1:
+ Input: ["5","2","C","D","+"]
+ Output: 30
+ Explanation:
+ Round 1: You could get 5 points. The sum is: 5.
+ Round 2: You could get 2 points. The sum is: 7.
+ Operation 1: The round 2's data was invalid. The sum is: 5.
+ Round 3: You could get 10 points (the round 2's data has been removed). The sum is: 15.
+ Round 4: You could get 5 + 10 = 15 points. The sum is: 30.
+
+
+ Example 2:
+ Input: ["5","-2","4","C","D","9","+","+"]
+ Output: 27
+ Explanation:
+ Round 1: You could get 5 points. The sum is: 5.
+ Round 2: You could get -2 points. The sum is: 3.
+ Round 3: You could get 4 points. The sum is: 7.
+ Operation 1: The round 3's data is invalid. The sum is: 3.
+ Round 4: You could get -4 points (the round 3's data has been removed). The sum is: -1.
+ Round 5: You could get 9 points. The sum is: 8.
+ Round 6: You could get -4 + 9 = 5 points. The sum is 13.
+ Round 7: You could get 9 + 5 = 14 points. The sum is 27.
+
+
+ Note:
+ The size of the input list will be between 1 and 1000.
+ Every integer represented in the list will be between -30000 and 30000.
+
+ * Created by WinnieZhao on 9/26/2017.
+ */
+public class BaseballGame {
+
+    public int calPoints(String[] ops) {
+        if (ops == null || ops.length == 0) {
+            return 0;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        int res = 0;
+        int num = 0;
+        for (String op : ops) {
+            if (op.equals("C")) {
+                num = -1 * Integer.valueOf(stack.pop());
+            }
+            else if (op.equals("D")) {
+                num = 2 * Integer.valueOf(stack.peek());
+                stack.push(num);
+            }
+            else if (op.equals("+")) {
+                int last1 = stack.pop();
+                int last2 = stack.pop();
+                num = last1 + last2;
+                stack.push(last2);
+                stack.push(last1);
+                stack.push(num);
+            }
+            else {
+                num = Integer.valueOf(op);
+                stack.push(num);
+            }
+            res += num;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        BaseballGame solution = new BaseballGame();
+        String[] ops = {"5","-2","4","C","D","9","+","+"};
+        System.out.println(solution.calPoints(ops));
+    }
+}
---
+++ LeetCodePractice/src/LeetCode/Interview/Amazon/KClosestPoints.java
@@ -0,0 +1,55 @@
+package LeetCode.Interview.Amazon;
+
+import LeetCode.Helper.Point;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Given some points and a point origin in two dimensional space, find k points out of the some points which are nearest to origin.
+ *
+ * Return these points sorted by distance, if they are same with distance, sorted by x-axis, otherwise sorted by y-axis.
+ *
+ * Example
+ * Given points = [[4,6],[4,7],[4,4],[2,5],[1,1]], origin = [0, 0], k = 3
+ * return [[1,1],[2,5],[4,4]]
+
+ */
+public class KClosestPoints {
+
+    public Point[] kClosest(Point[] points, Point original, int k) {
+
+        if(points == null) return points;
+        final Point origin = original;
+
+        PriorityQueue<Point> pq = new PriorityQueue<>(k, (a, b) -> {
+            int distanceA = (int) (Math.pow((a.x - origin.x),2) + Math.pow((a.y - origin.y), 2));
+            int distanceB = (int) (Math.pow((b.x - origin.x),2) + Math.pow((b.y - origin.y), 2));
+
+            int sub = distanceB - distanceA;
+            if (sub == 0) {
+                sub = b.x - a.x;
+                if (sub == 0) {
+                    sub = b.y - a.y;
+                }
+            }
+            return sub;
+        });
+
+        for (int i = 0; i < points.length; i++) {
+            pq.offer(points[i]);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        Point[] result = new Point[k];
+
+        int i = k - 1;
+        while (!pq.isEmpty()) {
+            result[i] = pq.poll();
+            i--;
+        }
+        return result;
+    }
+}
