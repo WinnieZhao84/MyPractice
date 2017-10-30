6534270e9f459c044b6bffe58b0bc2d782978c50
winniezhao <winnie.zhao@macgregorpartners.com>
2017-10-30 01:19:07 -0500

    20171029

--- LeetCodePractice/src/LeetCode/Medium/MeetingRoomsII.java
+++ LeetCodePractice/src/LeetCode/Medium/MeetingRoomsII.java
@@ -49,35 +49,24 @@ public class MeetingRoomsII {
     }
 
     public int minMeetingRooms_better(Interval[] intervals) {
-        if (intervals == null || intervals.length == 0)
+        if(intervals == null || intervals.length == 0) {
             return 0;
-
-        // Sort the intervals by start time
-        Arrays.sort(intervals, (a, b) -> a.start - b.start);
-
-        // Use a min heap to track the minimum end time of merged intervals
-        PriorityQueue<Interval> heap = new PriorityQueue<>(intervals.length, (a, b) -> a.end - b.end);
-
-        // start with the first meeting, put it to a meeting room
-        heap.offer(intervals[0]);
-
-        for (int i = 1; i < intervals.length; i++) {
-            // get the meeting room that finishes earliest
-            Interval interval = heap.poll();
-
-            if (intervals[i].start >= interval.end) {
-                // if the current meeting starts right after
-                // there's no need for a new room, merge the interval
-                interval.end = intervals[i].end;
-            } else {
-                // otherwise, this meeting needs a new room
-                heap.offer(intervals[i]);
-            }
-
-            // don't forget to put the meeting room back
-            heap.offer(interval);
         }
+        Arrays.sort(intervals, (i1, i2) -> i1.start - i2.start);
 
-        return heap.size();
+        // 用堆来管理房间的结束时间
+        PriorityQueue<Integer> endTimes = new PriorityQueue<>();
+        endTimes.offer(intervals[0].end);
+        
+        for(int i = 1; i < intervals.length; i++){
+            // 如果当前时间段的开始时间大于最早结束的时间，则可以更新这个最早的结束时间为当前时间段的结束时间，
+            // 如果小于的话，就加入一个新的结束时间，表示新的房间
+            if(intervals[i].start >= endTimes.peek()){
+                endTimes.poll();
+            }
+            endTimes.offer(intervals[i].end);
+        }
+        // 有多少结束时间就有多少房间
+        return endTimes.size();
     }
 }
