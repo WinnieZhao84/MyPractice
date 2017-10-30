cca367e4f96c79be844b1a695b51541f9f455d7d
winniezhao <winnie.zhao@macgregorpartners.com>
2017-10-29 23:22:04 -0500

    20171029

--- LeetCodePractice/src/LeetCode/Medium/UniquePaths.java
+++ LeetCodePractice/src/LeetCode/Medium/UniquePaths.java
@@ -20,22 +20,23 @@ public class UniquePaths {
             return 0;
         }
         
-        int[][] paths = new int[m+1][n+1];
-        
-        paths[0][0] = 0;
-        paths[0][1] = 0;
-        paths[1][0] = 0;
-        paths[1][1] = 1;
-        
-        for (int i=1; i<=m; i++) {
-            for (int j=1; j<=n; j++) {
-                if (i==j && i==1) continue;
+        int[][] paths = new int[m][n];
+
+        for (int i=0; i<m; i++) {
+            paths[i][0] = 1;
+        }
+
+        for (int i=0; i<n; i++) {
+            paths[0][i] = 1;
+        }
+
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
                 paths[i][j] = paths[i][j-1] + paths[i-1][j];
-                System.out.println(i + "-" + j + ": " + paths[i][j]);
             }
         }
         
-        return paths[m][n];
+        return paths[m-1][n-1];
         
     }
     
