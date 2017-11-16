6df3d0ccaaf1ef24817a19819c6bad0ea7e29771
Winnie Zhao <Winnie.Zhao@MacGregorPartners.com>
2017-11-16 02:32:23 -0600

    20171115

--- LeetCodePractice/src/LeetCode/Medium/UniquePathsII.java
+++ LeetCodePractice/src/LeetCode/Medium/UniquePathsII.java
@@ -22,31 +22,74 @@ package LeetCode.Medium;
  *
  */
 public class UniquePathsII {
-    
+
+    class Solution {
+        public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            if (obstacleGrid == null || obstacleGrid[0].length == 0) {
+                return 0;
+            }
+
+            int m = obstacleGrid.length;
+            int n = obstacleGrid[0].length;
+
+            int[][] dp = new int[m][n];
+
+            for (int i=0; i<m; i++) {
+                if (obstacleGrid[i][0] == 1) {
+                    dp[i][0] = 0;
+                    break;
+                }
+                else {
+                    dp[i][0] = 1;
+                }
+            }
+
+            for (int i=0; i<n; i++) {
+                if (obstacleGrid[0][i] == 1) {
+                    dp[0][i] = 0;
+                    break;
+                }
+                else {
+                    dp[0][i] = 1;
+                }
+            }
+
+            for (int i=1; i<m; i++) {
+                for (int j=1; j<n; j++) {
+                    if (obstacleGrid[i][j] == 1) {
+                        dp[i][j] = 0;
+                    }
+                    else {
+                        dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                    }
+                }
+            }
+            return dp[m-1][n-1];
+        }
+    }
+
+    // In place solution
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        
-        //Empty case
-        if(obstacleGrid.length == 0) return 0;
-        
-        int rows = obstacleGrid.length;
-        int cols = obstacleGrid[0].length;
-        
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(obstacleGrid[i][j] == 1)
-                    obstacleGrid[i][j] = 0;
-                else if(i == 0 && j == 0)
-                    obstacleGrid[i][j] = 1;
-                else if(i == 0)
-                    obstacleGrid[i][j] = obstacleGrid[i][j - 1] * 1;// For row 0, if there are no paths at left cell, then its 0,else 1
-                else if(j == 0)
-                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] * 1;// For col 0, if there are no paths at upper cell, then its 0,else 1
-                else
-                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        obstacleGrid[0][0] ^= 1;
+
+        for(int i=1;i<m; i++){
+            obstacleGrid[i][0] = (obstacleGrid[i][0]==1)?  0: obstacleGrid[i-1][0];
+        }
+
+        for(int j=1;j<n; j++){
+            obstacleGrid[0][j] = (obstacleGrid[0][j]==1)? 0 : obstacleGrid[0][j-1];
+        }
+
+        for(int i=1; i<m; i++) {
+            for(int j =1;j<n;j++) {
+                obstacleGrid[i][j] = (obstacleGrid[i][j]==1) ? 0 : obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
             }
         }
-        
-        return obstacleGrid[rows - 1][cols - 1];
+
+        return obstacleGrid[m-1][n-1];
     }
     
     public static void main(String[] args) {
