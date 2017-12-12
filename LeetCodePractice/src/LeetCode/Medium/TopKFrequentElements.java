f7fed46916fbabf9334600809dd70e43ae1e212d
Winnie Zhao <Winnie.Zhao@MacGregorPartners.com>
2017-12-12 01:56:14 -0600

    20171211

--- LeetCodePractice/src/LeetCode/Medium/TopKFrequentElements.java
+++ LeetCodePractice/src/LeetCode/Medium/TopKFrequentElements.java
@@ -1,10 +1,6 @@
 package LeetCode.Medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Given a non-empty array of integers, return the k most frequent elements.
@@ -22,24 +18,28 @@ import java.util.Map;
  */
 public class TopKFrequentElements {
 
+    /*
+     * Use an array to save numbers into different bucket whose index is the frequency
+     * O(n) solution
+     */
     public List<Integer> topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> frequencyMap = new HashMap<>();
 
         for (int n : nums) {
             frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
         }
-        
+
         List<Integer>[] bucket = new ArrayList[nums.length+1];
-        
+
         for (int key : frequencyMap.keySet()) {
             int frequency = frequencyMap.get(key);
             if (bucket[frequency] == null) {
                 bucket[frequency] = new ArrayList<>();
             }
             bucket[frequency].add(key);
-            
+
         }
-        
+
         List<Integer> res = new ArrayList<>();
 
         for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
@@ -48,6 +48,62 @@ public class TopKFrequentElements {
             }
         }
         return res;
+    }
+
+    /** Use max Heap. Put entry into maxHeap so we can always poll a number with largest frequency
+     *  O(NlogN) solution
+     */
+    public class Solution_I {
+        public List<Integer> topKFrequent(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int n: nums){
+                map.put(n, map.getOrDefault(n,0)+1);
+            }
+
+            PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((a, b) -> (b.getValue()-a.getValue()));
+            for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+                maxHeap.add(entry);
+            }
+
+            List<Integer> res = new ArrayList<>();
+            while(res.size()<k){
+                Map.Entry<Integer, Integer> entry = maxHeap.poll();
+                res.add(entry.getKey());
+            }
+            return res;
+        }
+    }
+
+    /**
+     *   Use TreeMap. Use frequency as the key so we can get all frequencies in order
+     *   O(NlogN) solution
+     *
+     *   TreeMap implementation provides guaranteed log(n) time cost for the containsKey, get, put and remove operations
+      */
+    public class Solution_II {
+        public List<Integer> topKFrequent(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for(int n: nums){
+                map.put(n, map.getOrDefault(n,0)+1);
+            }
+
+            TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
+            for(int num : map.keySet()){
+                int freq = map.get(num);
+                if(!freqMap.containsKey(freq)){
+                    freqMap.put(freq, new LinkedList<>());
+                }
+                freqMap.get(freq).add(num);
+            }
+
+            List<Integer> res = new ArrayList<>();
+            while(res.size()<k){
+                Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
+                res.addAll(entry.getValue());
+            }
+            return res;
+        }
     }
     
     public static void main (String[] args) {
