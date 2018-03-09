9c9a75457c2d10308efe34783807f0a2bed05f64
winniezhao <winnie.zhao@macgregorpartners.com>
2018-03-09 01:52:33 -0600

    20180308

--- LeetCodePractice/src/LeetCode/Interview/Microsoft/OA/LongestPathInDirectedAcyclicGraph.java
+++ LeetCodePractice/src/LeetCode/Interview/Microsoft/OA/LongestPathInDirectedAcyclicGraph.java
@@ -1,93 +1,138 @@
 package LeetCode.Interview.Microsoft.OA;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class LongestPathInDirectedAcyclicGraph {
 
-    public static class Graph {
-        private int[][] adjacencyMatrix;
-        private int numberOfVertices;
-
-        public Graph(int numberOfVertices) {
-            this.numberOfVertices = numberOfVertices;
-            this.adjacencyMatrix = new int[numberOfVertices][numberOfVertices];
-        }
-
-        public void addEdge(int source, int destination) {
-            adjacencyMatrix[source][destination] = 1;
-        }
-
-
-        public int getNumberOfVertices() {
-            return numberOfVertices;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < numberOfVertices; i++) {
-                List<Integer> list = new ArrayList<>();
-                for (int j = 0; j < numberOfVertices; j++) {
-                    if (adjacencyMatrix[i][j] == 1) {
-                        list.add(j);
-                    }
-                }
-                sb.append(i + " -> " + list + "\n");
-
-            }
-            return sb.toString();
-        }
-    }
-
-    public static Integer[] topologicalSortWithLongestPath(Graph graph) {
-        Integer[] longestPath = new Integer[graph.numberOfVertices];
-        Integer[] inDegree = new Integer[graph.numberOfVertices];
-
-        for (int destinationVertex = 0; destinationVertex < graph.numberOfVertices; destinationVertex++) {
-            inDegree[destinationVertex] = 0;
-            longestPath[destinationVertex] = 0;
-            for (int sourceVertex = 0; sourceVertex < graph.numberOfVertices; sourceVertex++) {
-                inDegree[destinationVertex] += graph.adjacencyMatrix[sourceVertex][destinationVertex];
-            }
-        }
-
-        for (int i = 0; i < graph.numberOfVertices; i++) {
-
-            if (inDegree[i] == 0) {
-                inDegree[i] = -1;
-                for (int k = 0; k < graph.numberOfVertices; k++) {
-                    if (graph.adjacencyMatrix[i][k] == 1) {
-                        inDegree[k] = inDegree[k] - 1;
-                        longestPath[k] = Math.max(longestPath[k], longestPath[k] + 1);
-                    }
-                }
-            }
-        }
-
-        return longestPath;
-    }
-
     public static void main(String[] args) {
-        Graph graph = new Graph(8);
-        graph.addEdge(0, 2);
-        graph.addEdge(0, 3);
-        graph.addEdge(0, 4);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 7);
-        graph.addEdge(2, 5);
-        graph.addEdge(3, 5);
-        graph.addEdge(3, 7);
-        graph.addEdge(4, 7);
-        graph.addEdge(5, 6);
-        graph.addEdge(6, 7);
+        Graph g = new Graph(6);
+        g.addEdge(0, 1, 5);
+        g.addEdge(0, 2, 3);
+        g.addEdge(1, 3, 6);
+        g.addEdge(1, 2, 2);
+        g.addEdge(2, 4, 4);
+        g.addEdge(2, 5, 2);
+        g.addEdge(2, 3, 7);
+        g.addEdge(3, 5, 1);
+        g.addEdge(3, 4, -1);
+        g.addEdge(4, 5, -2);
+        int s = 1;
+        g.findLongestPath(s);
+    }
 
-        System.out.println(graph);
+    public static class Graph {
+        private int V;
+        private int[][] matrix;
+        private int[] vertices;
+        private boolean[] visited;
+        private int[] distances;
+        private int[] predecessor;
+        private Stack<Integer> stack;
 
-        Integer[] longestPath = topologicalSortWithLongestPath(graph);
-        for (Integer vertex = 0; vertex < graph.getNumberOfVertices(); vertex++) {
-            System.out.println("Vertex(" + vertex+") longest path = " + longestPath[vertex]);
+        public Graph(int V) {
+            this.V = V;
+            vertices = new int[V];
+            visited = new boolean[V];
+            predecessor = new int[V];
+            distances = new int[V];
+            matrix = new int[V][V];
+            stack = new Stack<>();
+            for (int i = 0; i < V; i++) {
+                addVertex(i);
+                distances[i] = Integer.MIN_VALUE;
+                predecessor[i] = -1;
+            }
         }
+
+        private void addVertex(int name) {
+            vertices[name] = name;
+        }
+
+        public void addEdge(int source, int destination, int weight) {
+            matrix[source][destination] = weight;
+        }
+
+        public void findLongestPath(int source) {
+            invokeTopologicalSort();
+            distances[source] = 0; // Initialize source with 0
+            updateMaxDistanceForAllAdjVertices(); // for all nodes connected,
+            // directly or indirectly,
+            // with source will have
+            // their distances
+            // calculated
+            printDistances(source);
+            printPath(source);
+        }
+
+        private void printDistances(int source) {
+            System.out.println("Distances from source " + source + " are as follows: ");
+            for (int to = 0; to < V; to++) {
+                int distance = distances[to];
+                System.out.print("from " + source + " to " + to + ": ");
+                if (distance == Integer.MIN_VALUE) {
+                    System.out.println(" -Infinity ");
+                } else {
+                    System.out.println(distance + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        private void printPath(int source) {
+            System.out.println("Path from source " + source + " to other nodes are as follows: ");
+            for (int i = 0; i < V; i++) {
+                if (distances[i] == Integer.MIN_VALUE) {
+                    System.out.println("No Path from " + source + " to " + i);
+                } else if (i != source) {
+                    int from = predecessor[i];
+                    System.out.print("Path from " + source + " to " + i + ": ");
+                    System.out.print(i + " ");
+                    if (from == source) {
+                        System.out.print(from + " ");
+                    }
+                    while (from != source) {
+                        System.out.print(from + " ");
+                        from = predecessor[from];
+                    }
+                    System.out.print(source);
+                    System.out.println();
+                }
+            }
+        }
+
+        private void updateMaxDistanceForAllAdjVertices() {
+            while (!stack.isEmpty()) {
+                Integer from = stack.pop();
+                if (distances[from] != Integer.MIN_VALUE) {
+                    for (int adjacent = 0; adjacent < V; adjacent++) {
+                        if (matrix[from][adjacent] != 0) {
+                            if (distances[adjacent] < distances[from] + matrix[from][adjacent]) {
+                                predecessor[adjacent] = from;
+                                distances[adjacent] = distances[from] + matrix[from][adjacent];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void invokeTopologicalSort() {
+            for (int i = 0; i < V; i++) {
+                if (!visited[i]) {
+                    dfs(i);
+                }
+            }
+        }
+
+        private void dfs(int source) {
+            visited[source] = true;
+            for (int adjacent = 0; adjacent < V; adjacent++) {
+                if (matrix[source][adjacent] != 0 && !visited[adjacent]) {
+                    dfs(adjacent);
+                }
+            }
+            stack.push(source);
+        }
+
     }
 }
