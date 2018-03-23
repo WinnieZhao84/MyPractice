cf9cf0e24bb0a970dd0dcde0001b105f02b268c5
winniezhao <winnie.zhao@macgregorpartners.com>
2018-03-23 11:02:11 -0500

    20180322

---
+++ LeetCodePractice/src/LeetCode/Interview/Microsoft/LeetCode/SearchRangeInBinarySearchTree.java
@@ -0,0 +1,38 @@
+package LeetCode.Interview.Microsoft.LeetCode;
+
+import LeetCode.Helper.TreeNode;
+
+import java.util.ArrayList;
+
+/**
+ * LintCode 11
+ */
+public class SearchRangeInBinarySearchTree {
+
+    private ArrayList<Integer> results;
+    /**
+     * @param root: The root of the binary search tree.
+     * @param k1 and k2: range k1 to k2.
+     * @return: Return all keys that k1<=key<=k2 in increasing order.
+     */
+    public ArrayList<Integer> searchRange(TreeNode root, int k1, int k2) {
+        results = new ArrayList<>();
+        helper(root, k1, k2);
+        return results;
+    }
+
+    private void helper(TreeNode root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+        if (root.val > k1) {
+            helper(root.left, k1, k2);
+        }
+        if (root.val >= k1 && root.val <= k2) {
+            results.add(root.val);
+        }
+        if (root.val < k2) {
+            helper(root.right, k1, k2);
+        }
+    }
+}
