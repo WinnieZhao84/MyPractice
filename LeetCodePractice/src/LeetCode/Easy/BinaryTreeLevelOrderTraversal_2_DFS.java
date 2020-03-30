2109bf1eff151385547442040aeff7b9b5e860bd
Winnie Zhao <Winnie.Zhao@MacGregorPartners.com>
2020-03-29 22:58:45 -0700

    20200329

--- LeetCodePractice/src/LeetCode/Easy/BinaryTreeLevelOrderTraversal_2_DFS.java
+++ LeetCodePractice/src/LeetCode/Easy/BinaryTreeLevelOrderTraversal_2_DFS.java
@@ -28,27 +28,26 @@ return its bottom-up level order traversal as:
  */
 public class BinaryTreeLevelOrderTraversal_2_DFS {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        
-        this.levelOrder(root, 0, result);
-        Collections.reverse(result);
-        
+
+        List<List<Integer>> result = new ArrayList<>();
+        this.dfs(result, root, 0);
+
         return result;
     }
-
-    private void levelOrder(TreeNode root, int depth, List<List<Integer>> result) {
-        if (root != null) {
-            if (result.size() == 0 || result.size() <= depth) {
-                List<Integer> sameLevelList = new ArrayList<Integer>();
-                sameLevelList.add(root.val);
-                result.add(sameLevelList);
-            }
-            else {
-                result.get(depth).add(root.val);
-            }
-            
-            levelOrder(root.left, depth+1, result);
-            levelOrder(root.right, depth+1, result);
+    
+    private void dfs(List<List<Integer>> result, TreeNode node, int height) {
+        if (node == null) {
+            return;
         }
+        
+        if (height >= result.size()) {
+            result.add(0, new ArrayList<>());
+        }
+
+        
+        dfs(result, node.left, height + 1);
+        dfs(result, node.right, height + 1);
+        
+        result.get(result.size()-height-1).add(node.val);
     }
 }
