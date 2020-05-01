5d62f013381393e8db550b7f85755f848391a611
WinnieZhao84 <WinnieZhao84@users.noreply.github.com>
2020-05-01 01:02:07 -0700

    Update NumberOfIslands.java

--- LeetCodePractice/src/LeetCode/Medium/NumberOfIslands.java
+++ LeetCodePractice/src/LeetCode/Medium/NumberOfIslands.java
@@ -28,29 +28,96 @@ package LeetCode.Medium;
  */
 public class NumberOfIslands {
 
-    public int numIslands(char[][] grid) {
-        if(grid==null || grid.length==0) return 0;
+   class Island {
+        int x;
+        int y;
         
-        int count = 0;
-        for (int i=0; i<grid.length; i++) {
-            for (int j=0; j<grid[0].length; j++) {
-                if (grid[i][j]=='1') {
-                    this.dfsFill(grid, i, j);
-                    count++;
+        Island (int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    // BFS
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        boolean[][] visited = new boolean[m][n];
+       
+        int num = 0;
+        int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
+        
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+
+                if (grid[i][j] == '1') {
+                    Queue<Island> queue = new LinkedList<>();        
+                    
+                    Island island = new Island(i, j);
+                    queue.add(island);
+                    visited[i][j] = true;
+                    
+                    while (!queue.isEmpty()) {
+                        Island cur = queue.poll();
+                        
+                        for (int k=0; k<4; k++) {
+                            int x = cur.x + directions[k][0];
+                            int y = cur.y + directions[k][1];
+                            
+                            if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == '1' && !visited[x][y]) {
+                                grid[x][y] = '0';
+                                visited[x][y] = true;
+                                queue.offer(new Island(x, y));
+                            }
+                        }
+                    }
+                    num++;
+                }
+
+            }
+        }
+        return num;
+    }
+
+    public int numIslands_dfs(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        boolean[][] visited = new boolean[m][n];
+        
+        int num = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                    dfs(grid, i, j, m, n, visited);
                 }
             }
         }
-        return count;
+        return num;
     }
     
-    private void dfsFill(char[][] grid, int i, int j) {
-        if (i>=0 && j>=0 && i<grid.length && j<grid[0].length && grid[i][j] == '1') {
-            grid[i][j] = '0';
-            
-            dfsFill(grid, i + 1, j);
-            dfsFill(grid, i - 1, j);
-            dfsFill(grid, i, j + 1);
-            dfsFill(grid, i, j - 1);
+    private void dfs(char[][] grid, int x, int y, int m, int n, boolean[][] visited) {
+        if (x < 0 || y < 0 || x >=m || y >= n || visited[x][y] || grid[x][y] == '0') {
+            return;
         }
+        
+        grid[x][y] = '0';
+        visited[x][y] = true;
+        
+        dfs(grid, x-1, y, m, n, visited);
+        dfs(grid, x+1, y, m, n, visited);
+        dfs(grid, x, y-1, m, n, visited);
+        dfs(grid, x, y+1, m, n, visited);
+        
     }
 }
