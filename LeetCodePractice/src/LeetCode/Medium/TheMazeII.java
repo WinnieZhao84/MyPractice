385bb64a0d471d80a8a819d5931ce4fbd8667335
WinnieZhao84 <WinnieZhao84@users.noreply.github.com>
2020-05-14 01:50:41 -0700

    Update TheMazeII.java

--- LeetCodePractice/src/LeetCode/Medium/TheMazeII.java
+++ LeetCodePractice/src/LeetCode/Medium/TheMazeII.java
@@ -30,6 +30,10 @@ public class TheMazeII {
     
     int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
     
+    // Time complexity : O(m*n*max(m,n)). Complete traversal of maze will be done in the worst case. 
+    // Here, mm and nn refers to the number of rows and columns of the maze. Further, for every current node chosen, 
+    // we can travel upto a maximum depth of max(m,n) in any direction.
+    // Space complexity : O(mn). distance array of size m*n is used.     
     public int shortestDistance(int[][] maze, int[] start, int[] destination) {
         if (maze == null || maze.length == 0) {
             return -1;
@@ -75,5 +79,58 @@ public class TheMazeII {
             }
         }
         return distance[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1 : distance[destination[0]][destination[1]];
+    }
+  
+    // Time complexity : O(m*n*max(m,n)). Complete traversal of maze will be done in the worst case. 
+    // Here, mm and nn refers to the number of rows and columns of the maze. Further, for every current node chosen, 
+    // we can travel upto a maximum depth of max(m,n) in any direction.
+    // Space complexity : O(mn). distance array of size m*n is used. 
+    public int shortestDistance_DFS(int[][] maze, int[] start, int[] destination) {
+        if (maze == null || maze.length == 0) {
+            return -1;
+        }
+        
+        Queue<Cell> queue = new LinkedList<>();
+        queue.add(new Cell(start[0], start[1]));
+       
+        int m = maze.length;
+        int n = maze[0].length;
+        
+        // two matrix minimum value, common approach is to keep a DP matrix as record
+        int[][] distance = new int[m][n];
+        for (int[] row: distance) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+            
+        distance[start[0]][start[1]] = 0;
+        
+        dfs(maze, start, distance);
+        
+        return distance[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1 : distance[destination[0]][destination[1]];
+    }
+    
+    private void dfs(int[][] maze, int[] cur, int[][] distance) {
+        for (int[] dir : dirs) {
+            int x = cur[0];
+            int y = cur[1];
+        
+            int step = 0;
+            while (x>=0 && y>=0 && x<maze.length && y<maze[0].length && maze[x][y] == 0) {
+                x+=dir[0];
+                y+=dir[1];
+                
+                step++;
+            }
+                            
+            int lastX = x - dir[0];
+            int lastY = y - dir[1];
+            step--;
+                
+            if (distance[lastX][lastY] > distance[cur[0]][cur[1]] + step) {
+                distance[lastX][lastY] = distance[cur[0]][cur[1]] + step;
+                dfs(maze, new int[]{lastX,lastY}, distance);
+            }
+        }
+        return;
     }
 }
